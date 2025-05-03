bbea84069e72010b9cf79d01d911238590b13b95
AISMP-DEV <devanonim.xxsmp@gmail.com>
2025-05-03 07:37:37 -0300

    Tornei Logger publico.

--- src/main/java/net/aqualoco/cantinhospooky/CantinhoSpooky.java
+++ src/main/java/net/aqualoco/cantinhospooky/CantinhoSpooky.java
@@ -17,11 +17,9 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CantinhoSpooky.MOD_ID)
 public class CantinhoSpooky {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "cantinhospooky";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public static final Logger LOGGER = LogUtils.getLogger();
+
     public CantinhoSpooky(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
